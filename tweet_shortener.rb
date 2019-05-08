46f0652ed81f3a5556bbed1a2e16e50e12b4ec60
Travis Exline <t.exline22@gmail.com>
2019-05-08 15:18:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,3 +29,11 @@ def bulk_tweet_shortener(tweets_array)
     puts word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    puts word_substituter(tweet)
+  else
+    puts tweet
+  end
+end
